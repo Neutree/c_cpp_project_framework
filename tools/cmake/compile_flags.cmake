7c00f74c0e66a097a1e96984d805818ab8e7e07f
Neucrack <CZD666666@gmail.com>
2019-06-24 01:36:13 +0800

    compile on windows with minGW

--- tools/cmake/compile_flags.cmake
+++ tools/cmake/compile_flags.cmake
@@ -8,7 +8,7 @@ set(CMAKE_C_FLAGS "-Wall")
 set(CMAKE_CXX_FLAGS "-Wall")
 ################################
 
-set(LDFLAGS )
+set(LDFLAGS "")
 set(CMAKE_EXE_LINKER_FLAGS  "")
 set(CMAKE_SHARED_LINKER_FLAGS "")
 set(CMAKE_MODULE_LINKER_FLAGS "")
